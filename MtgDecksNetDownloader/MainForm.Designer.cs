2559b76fda76299e1560f457afa157366cde5133
Dessyreqt <carroll.david.s@gmail.com>
2015-11-12 08:36:18 -0600

    Initial commit
    Initial commit

---
+++ MtgDecksNetDownloader/MainForm.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace MtgDecksNetDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputFolderText = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.eventAddressText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.getDecksButton = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.outputDeckText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.order = new System.Windows.Forms.NumericUpDown();
+            this.outputFileText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.seedDeckText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buildDeckButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.order)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Output Folder";
+            // 
+            // outputFolderText
+            // 
+            this.outputFolderText.Location = new System.Drawing.Point(16, 30);
+            this.outputFolderText.Name = "outputFolderText";
+            this.outputFolderText.Size = new System.Drawing.Size(268, 20);
+            this.outputFolderText.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(290, 27);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // eventAddressText
+            // 
+            this.eventAddressText.Location = new System.Drawing.Point(16, 70);
+            this.eventAddressText.Name = "eventAddressText";
+            this.eventAddressText.Size = new System.Drawing.Size(268, 20);
+            this.eventAddressText.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "MtgDecks.Net Event Address";
+            // 
+            // getDecksButton
+            // 
+            this.getDecksButton.Location = new System.Drawing.Point(290, 67);
+            this.getDecksButton.Name = "getDecksButton";
+            this.getDecksButton.Size = new System.Drawing.Size(75, 23);
+            this.getDecksButton.TabIndex = 5;
+            this.getDecksButton.Text = "Get Decks";
+            this.getDecksButton.UseVisualStyleBackColor = true;
+            this.getDecksButton.Click += new System.EventHandler(this.getDecksButton_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(16, 96);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.ReadOnly = true;
+            this.outputText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputText.Size = new System.Drawing.Size(349, 269);
+            this.outputText.TabIndex = 6;
+            // 
+            // outputDeckTextBox
+            // 
+            this.outputDeckText.Location = new System.Drawing.Point(371, 214);
+            this.outputDeckText.Multiline = true;
+            this.outputDeckText.Name = "outputDeckText";
+            this.outputDeckText.ReadOnly = true;
+            this.outputDeckText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.outputDeckText.Size = new System.Drawing.Size(276, 151);
+            this.outputDeckText.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(368, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ranking Order";
+            // 
+            // order
+            // 
+            this.order.Location = new System.Drawing.Point(371, 31);
+            this.order.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.order.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.order.Name = "order";
+            this.order.Size = new System.Drawing.Size(73, 20);
+            this.order.TabIndex = 9;
+            this.order.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // outputFileText
+            // 
+            this.outputFileText.Location = new System.Drawing.Point(453, 30);
+            this.outputFileText.Name = "outputFileText";
+            this.outputFileText.Size = new System.Drawing.Size(194, 20);
+            this.outputFileText.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(450, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Output File (.txt will be appended)";
+            // 
+            // seedDeckText
+            // 
+            this.seedDeckText.Location = new System.Drawing.Point(371, 85);
+            this.seedDeckText.Multiline = true;
+            this.seedDeckText.Name = "seedDeckText";
+            this.seedDeckText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.seedDeckText.Size = new System.Drawing.Size(276, 123);
+            this.seedDeckText.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(368, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Seed Deck";
+            // 
+            // buildDeckButton
+            // 
+            this.buildDeckButton.Location = new System.Drawing.Point(572, 56);
+            this.buildDeckButton.Name = "buildDeckButton";
+            this.buildDeckButton.Size = new System.Drawing.Size(75, 23);
+            this.buildDeckButton.TabIndex = 14;
+            this.buildDeckButton.Text = "Build Deck";
+            this.buildDeckButton.UseVisualStyleBackColor = true;
+            this.buildDeckButton.Click += new System.EventHandler(this.buildDeckButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 377);
+            this.Controls.Add(this.buildDeckButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.seedDeckText);
+            this.Controls.Add(this.outputFileText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.order);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputDeckText);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.getDecksButton);
+            this.Controls.Add(this.eventAddressText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.outputFolderText);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "MtgDecks.Net Downloader";
+            ((System.ComponentModel.ISupportInitialize)(this.order)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox outputFolderText;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox eventAddressText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button getDecksButton;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.TextBox outputDeckText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown order;
+        private System.Windows.Forms.TextBox outputFileText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox seedDeckText;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buildDeckButton;
+    }
+}
+
---
+++ MtgDecksNetDownloader/MainForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Net;
+using System.Windows.Forms;
+using MtgDecksNetDownloader.Properties;
+
+namespace MtgDecksNetDownloader
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+        private void LoadSettings()
+        {
+            outputFolderText.Text = Settings.Default.OutputFolder;
+            eventAddressText.Text = Settings.Default.EventAddress;
+            order.Value = Settings.Default.Order;
+            outputFileText.Text = Settings.Default.OutputDeck;
+            seedDeckText.Text = Settings.Default.SeedDeck;
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            var browser = new FolderBrowserDialog();
+            browser.SelectedPath = outputFolderText.Text;
+
+            if (browser.ShowDialog() == DialogResult.OK)
+            {
+                outputFolderText.Text = browser.SelectedPath;
+                Settings.Default.OutputFolder = browser.SelectedPath;
+                Settings.Default.Save();
+            }
+        }
+
+        private void getDecksButton_Click(object sender, EventArgs e)
+        {
+            FixOutputFolder();
+
+            outputText.Text = "";
+
+            Settings.Default.EventAddress = eventAddressText.Text;
+            Settings.Default.Save();
+
+            var response = GetResponse(eventAddressText.Text);
+
+            if (string.IsNullOrWhiteSpace(response))
+                return;
+
+            var urlPattern = new Regex(@"/decks/view/\d+");
+            var matches = urlPattern.Matches(response);
+            var deckUrls = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                var deckUrl = string.Format("http://mtgdecks.net{0}/txt", match.Value);
+
+                deckUrls.Add(deckUrl);
+                outputText.Text += string.Format("{0}{1}", deckUrl, Environment.NewLine);
+            }
+
+            var deckNum = 0;
+
+            foreach (var deckUrl in deckUrls)
+            {
+                var deckList = GetResponse(deckUrl);
+
+                while (File.Exists(string.Format("{0}{1}.txt", outputFolderText.Text, deckNum.ToString("000"))))
+                {
+                    deckNum++;
+                }
+
+                using (var writer = new StreamWriter(string.Format("{0}{1}.txt", outputFolderText.Text, deckNum.ToString("000"))))
+                {
+                    writer.Write(deckList);
+                }
+            }
+        }
+
+        private void FixOutputFolder()
+        {
+            if (!outputFolderText.Text.EndsWith("\\"))
+            {
+                outputFolderText.Text += "\\";
+                Settings.Default.OutputFolder = outputFolderText.Text;
+                Settings.Default.Save();
+            }
+        }
+
+        private static string GetResponse(string address)
+        {
+            var httpRequest = WebRequest.Create(address);
+            var response = httpRequest.GetResponse();
+            var reader = new StreamReader(response.GetResponseStream());
+            return reader.ReadToEnd();
+        }
+
+        private void buildDeckButton_Click(object sender, EventArgs e)
+        {
+            FixOutputFolder();
+            DeleteTempFile();
+
+            Settings.Default.Order = (int)order.Value;
+            Settings.Default.OutputDeck = outputFileText.Text;
+            Settings.Default.SeedDeck = seedDeckText.Text;
+            Settings.Default.Save();
+
+            if (!Directory.Exists(string.Format("{0}\\output", outputFolderText.Text)))
+            {
+                Directory.CreateDirectory(string.Format("{0}\\output", outputFolderText.Text));
+            }
+
+            if (string.IsNullOrWhiteSpace(outputFileText.Text))
+            {
+                MessageBox.Show("Need an output file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                return;
+            }
+
+            var args = new StringBuilder();
+
+            args.Append("-v");
+            args.AppendFormat(" -r {0}", order.Value);
+            args.AppendFormat(" -o output\\{0}.txt", outputFileText.Text);
+            
+            if (!string.IsNullOrWhiteSpace(seedDeckText.Text))
+            {
+                using (var writer = new StreamWriter(string.Format("{0}_.tmp", outputFolderText.Text)))
+                {
+                    writer.Write(seedDeckText.Text);
+                }
+
+                args.Append(" -i _.tmp");
+            }
+
+            foreach (var filename in Directory.EnumerateFiles(outputFolderText.Text, "*.txt", SearchOption.TopDirectoryOnly))
+            {
+                args.AppendFormat(" {0}", filename.Substring(filename.LastIndexOf('\\') + 1));
+            }
+
+
+            Process proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "mtg-builder",
+                    Arguments = args.ToString(),
+                    //UseShellExecute = false,
+                    //RedirectStandardOutput = true,
+                    //CreateNoWindow = true,
+                    WorkingDirectory = outputFolderText.Text,
+                }
+            };
+
+            proc.Start();
+            outputDeckText.Text = "";
+
+            //while (!proc.StandardOutput.EndOfStream)
+            //{
+            //    string line = proc.StandardOutput.ReadLine();
+            //    outputDeckText.Text += line;
+            //    Application.DoEvents();
+            //}
+
+            DeleteTempFile();
+        }
+
+        private void DeleteTempFile()
+        {
+            var filename = string.Format("{0}_.txt", outputFolderText.Text);
+
+            File.Delete(filename);
+        }
+    }
+}
