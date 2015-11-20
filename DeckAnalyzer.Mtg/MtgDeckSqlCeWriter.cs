fd5c76994614e3183da8368a8a76d04873d12427
David Carroll <carroll.david.s@gmail.com>
2015-11-20 09:20:55 -0600

    Sql CE start

---
+++ DeckAnalyzer.Mtg/MtgDeckSqlCeWriter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DeckAnalyzer.Common;
+using DeckAnalyzer.Data;
+
+namespace DeckAnalyzer.Mtg
+{
+    public class MtgDeckSqlCeWriter : IDeckDatabaseWriter
+    {
+        public string ConnectionString { get; set; }
+
+        public void WriteDeck(IDeck deck)
+        {
+            if (ConnectionString == null)
+            {
+                throw new InvalidOperationException("The ConnectionString property must be set before calling WriteDeck.");
+            }
+
+            
+        }
+    }
+}
