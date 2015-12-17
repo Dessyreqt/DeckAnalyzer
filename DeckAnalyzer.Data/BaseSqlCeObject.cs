5cc84ab8bfe4642732b78d228baef5ebf873e9e7
David Carroll <carroll.david.s@gmail.com>
2015-12-17 09:45:24 -0600

    Add BaseSqlCeObject

---
+++ DeckAnalyzer.Data/BaseSqlCeObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeckAnalyzer.Data
+{
+    public abstract class BaseSqlCeObject
+    {
+    }
+}
--- DeckAnalyzer.Mtg/MtgDeckSqlCeAnalyzer.cs
+++ DeckAnalyzer.Mtg/MtgDeckSqlCeAnalyzer.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlServerCe;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using DeckAnalyzer.Data;
 
 namespace DeckAnalyzer.Mtg
@@ -13,7 +17,37 @@ namespace DeckAnalyzer.Mtg
 
         public void AnalyzeDecks(int order)
         {
-            throw new NotImplementedException();
+            if (ConnectionString == null)
+            {
+                throw new InvalidOperationException("The ConnectionString property must be set before calling WriteDeck.");
+            }
+
+            var filename = GetFileNameFromConnectionString();
+            var deckIndex = -1;
+
+            if (!File.Exists(filename))
+            {
+                throw new InvalidOperationException("The Database must be created.");
+            }
+            using (var conn = new SqlCeConnection(ConnectionString))
+            {
+                conn.Open();
+
+            }
+        }
+
+        private string GetFileNameFromConnectionString()
+        {
+            var regex = new Regex(@"DataSource=""(?<filename>[^/*?\""<>|]+)""");
+
+            Match match = regex.Match(ConnectionString);
+
+            if (match.Success)
+            {
+                return match.Groups["filename"].Value;
+            }
+
+            throw new InvalidOperationException("Could not get database filename from connection string. Please verify the connection string and retry.");
         }
     }
 }
